eb76d502b0edbc571594e1685255a48ee9558abe
Marco Vassena <m.vassena@students.uu.nl>
2014-11-19 19:12:33 +0100

    Ported core module to use parsec

--- src/Format/Types.hs
+++ src/Format/Types.hs
@@ -3,14 +3,28 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Format.Types where
 
-import Data.Attoparsec.Types
-import Data.Attoparsec.Combinator
+import Text.Parsec.ByteString ()
+import Text.Parsec.Text ()
+import Text.Parsec
+import Text.Parsec.Prim
+import Text.Parsec.Combinator
+import Text.Parsec.Char
 import Data.Monoid
 import Data.Proxy
-import Control.Applicative
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
+import Data.Text (Text)
+import qualified Data.Text as T
+import Control.Applicative ((<$>), (<*>), (*>), (<*))
+import Control.Monad.Identity
+import GHC.TypeLits
+
+type Parser i a = Parsec i () a
 
 -- Sequence
 data a :*: b = a :*: b
@@ -36,6 +50,19 @@ data a :~>: b = a :~>: b
 --  Nil :: Vector a 0
 --  Cons :: a -> Vector a n -> Vector a (n + 1)
 
+-- Used to parse a format described in a DataFormat instance.
+-- It requires the whole input to be consumed.
+parseFormat :: (DataFormat i a, Stream i Identity ()) => Parser i a -> i -> Either ParseError a
+parseFormat p = parse (p <* eof) ""
+
+parseTest :: (DataFormat i a, Stream i Identity (), Show i) => Parser i a -> i -> IO ()
+parseTest p input = 
+  case parseFormat p input of
+    Left err -> do 
+      putStr "parse error at "
+      print err
+    Right x -> putStr $ show $ (encode x) `asTypeOf` input
+
 -- Defines the encoding / decoding functions for a DataFormat
 class DataFormat i a where
   decode :: Parser i a
@@ -58,9 +85,49 @@ instance (Monoid i, DataFormat i a) => DataFormat i (Some a) where
   decode = Some <$> decode <*> many decode
   encode (Some a as) = mconcat (fmap encode (a : as))
 
+--------------------------------------------------------------------------------
+-- ByteString instances
+
+instance DataFormat ByteString Char where
+  decode = anyChar
+  encode = B.singleton
+
+instance (KnownSymbol s, DecodeKindLit ByteString s) => DataFormat ByteString (Proxy s) where
+  decode = decodeKind Proxy
+  encode = B.pack . symbolVal
+
+instance (KnownNat n, DecodeKindLit ByteString n) => DataFormat ByteString (Proxy n) where
+  decode = decodeKind Proxy
+  encode = B.pack . show . natVal 
+
+--------------------------------------------------------------------------------
+-- Text instances 
+
+instance DataFormat Text Char where
+  decode = anyChar
+  encode = T.singleton
+
+instance (KnownSymbol s, DecodeKindLit Text s) => DataFormat Text (Proxy s) where
+  decode = decodeKind Proxy
+  encode = T.pack . symbolVal
+
+instance (KnownNat n, DecodeKindLit Text n) => DataFormat Text (Proxy n) where
+  decode = decodeKind Proxy
+  encode = T.pack . show . natVal 
+
+--------------------------------------------------------------------------------
 -- An auxiliary class used to recognize type level strings and numbers.
 class DecodeKindLit i s  where
   decodeKind :: Proxy s -> Parser i (Proxy s)
+
+instance (Stream i Identity Char, KnownSymbol s) => DecodeKindLit i s where
+  decodeKind p = string (symbolVal p) *> return Proxy
+
+instance (Stream i Identity Char, KnownNat s) => DecodeKindLit i s where
+  -- TODO not sure if this is a sensible definition
+  decodeKind p = string (show (natVal p)) *> return Proxy
+
+--------------------------------------------------------------------------------
 
 -- An auxiliary class that abstracs context sensitive decoding.
 -- The user is expected to implement this instance.
