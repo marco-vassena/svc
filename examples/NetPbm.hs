c2a2aa308254d2e385b65ffc39a221c59da21860
Marco Vassena <m.vassena@students.uu.nl>
2015-01-15 23:03:52 +0100

    Updated NetPbm example

--- examples/NetPbm.hs
+++ examples/NetPbm.hs
@@ -1,104 +1,61 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE InstanceSigs #-}
 
 module NetPbm where
 
-import Control.Applicative
+import Prelude hiding ((>>=))
+import Control.Isomorphism.Partial
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B
-import Data.Foldable
+import Data.HList
 import Data.List
-import Data.Proxy
 import Data.Word
+
 import Format.Base
-import Format.DataFormat
-import Format.Decode hiding (Parser)
-import Format.Encode
-import Text.Parsec.Char
-import Text.Parsec.Combinator
-import Text.Parsec.ByteString
+import Format.Combinator
+import Format.Token
+import Format.Parser
+import Format.Parser.Parsec
 
--- I define a new data-type so that I can write an instance for 
--- DataFormat in which precisely only "1" and "0" are recognized
-data Bit = Zero | One
+import Text.Parsec.String
 
-instance Encode ByteString Bit where
-  gencode Zero = "0"
-  gencode One = "1"
+-- TODO Move this to char module
+int :: SFormat m Char Int
+int = i <$> some digit
+  where i :: Iso '[String] '[Int] 
+        i = Iso f g (SCons SNil) (SCons SNil)
+          where f :: HList '[ String ] -> Maybe (HList '[Int])
+                f (Cons s _) = Just . hsingleton $ read s
+                g :: HList '[ Int ] -> Maybe (HList '[String])
+                g (Cons n _) = Just . hsingleton $ show n
 
-instance Decode ByteString Bit where
-  gdecode = zeroP <|> oneP
-    where zeroP = char '1' *> return One
-          oneP = char '0' *> return Zero 
+-- TODO move to char module
+spaces :: Format m Char '[]
+spaces = some (char ' ' <|> char '\t' <|> char '\n')
 
-data Image = Image [[Bit]]
+data Pbm = Pbm Int Int [[Char]]
+  deriving Show
 
--- The format specification states that no line should be longer than 70 characters.
-instance Encode ByteString Image where
-  gencode (Image img) = B.unlines . trim70 $ map (B.unwords . map encode) img
-    where trim70 :: [ a ] -> [ a ]
-          trim70 bits = case splitAt 70 bits of
-                          (pre, []) -> pre
-                          (pre, suf) -> pre ++ trim70 suf
+pbm :: Iso '[Int, Int, [[Char]]] '[Pbm]
+pbm = Iso (Just . hsingleton . happly Pbm) f (SCons (SCons (SCons SNil))) (SCons SNil)
+  where f :: PFunction '[Pbm] '[Int, Int, [[Char]]]
+        f (Cons (Pbm n m img) _) = Just $ Cons n (Cons m (Cons img Nil))
 
-type WhiteSpace = Many (Proxy " " :+: Proxy "\t" :+: Proxy "\n" :+: Proxy "\r")
-type PbmMagic = Proxy "P1\n" 
-type Comment = Proxy "#" :*: Many (NoneOf "\n") :*: Proxy "\n"
-type PbmHeader = PbmMagic :*: Maybe (Comment) :*: WhiteSpace :*: Int :*: WhiteSpace :*: Int :*: WhiteSpace
--- Ascii Portable Bit map
-type Pbm = PbmHeader :~>: Image
+pbmFormat :: SFormat m Char Pbm
+pbmFormat = pbm <$> pbmRawFormat
 
--- Maybe I could use datatypes a la carte to provide a sort of search mechanism,
--- so that the user can specify only the part of the header that he really needs.
--- In this example an instance for Int :*: WhiteSpace :*: Int would be enough.
-instance DecodeWith ByteString PbmHeader Image where
-  decodeWith :: PbmHeader -> Parser Image
-  decodeWith (_ :*: _ :*:  _ :*: width :*: _ :*: height :*: _) = Image <$> count height parseRow
-    where parseRow :: Parser [Bit]
-          parseRow = count width (decode <* spaces)
+pbmRawFormat :: Format m Char '[Int, Int, [[Char]]]
+pbmRawFormat = pbmHeader >>= \(Cons n (Cons m _)) -> img n m
 
---------------------------------------------------------------------------------
+pbmHeader :: Format m Char '[Int, Int]
+pbmHeader = (string "P1\n" @> int <@ spaces) <@> int <@ spaces
+        -- comment = char '#' @> many noneOf "\n" <@ char "\n"
 
-data PgmImage = PgmImage [[ Word8 ]]
+img :: Int -> Int -> SFormat m Char [[Char]]
+img n m = count n (count m (bit <@ spaces))
+  where bit = oneOf "01"
 
-type PgmMagic = Proxy "P5" :*: WhiteSpace
-type PgmHeader = Int :*: WhiteSpace :*: Int :*: WhiteSpace :*: Int :*: WhiteSpace
--- Binary Portable Gray Map
-type Pgm = PgmMagic :*: Maybe Comment :*: (PgmHeader :~>: PgmImage)
-
-instance Encode ByteString PgmImage where
-  gencode (PgmImage img) = foldMap (foldMap gencode) img
-
-instance DecodeWith ByteString PgmHeader PgmImage where
-  decodeWith (width :*: _ :*: height :*: _ :*: maxVal :*: _) = 
-    PgmImage <$> count height (count width decode)
-
-parsePbm :: Parser Pbm
-parsePbm = decode
-
-parsePgm :: Parser Pgm
-parsePgm = decode
-
-data ImageType = PbmType
-               | PgmType
-
-getImageType :: String -> ImageType
-getImageType s | "ascii.pbm" `isSuffixOf` s = PbmType
-getImageType s | "pgm" `isSuffixOf` s = PgmType
-getImageType s | otherwise = error $ "Unsupported format: " ++ s
-
-report :: (DataFormat ByteString a, Show e) => Either e a -> IO ()
-report = either print (B.putStr . encode)
-
-test :: String -> IO ()
-test f = do
-  s <- B.readFile f
-  case getImageType f of
-    PbmType -> report $ parseFormat parsePbm s
-    PgmType -> report $ parseFormat parsePgm s
+pbmParser :: Parser (HList '[Pbm])
+pbmParser = mkParser pbmFormat
