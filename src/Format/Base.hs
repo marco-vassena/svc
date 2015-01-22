094cf4405b8a9c06dc8845f437e8e2801fb711ff
Marco Vassena <m.vassena@students.uu.nl>
2015-01-22 12:27:03 +0100

    Changed Format.Base for new format encoding
      Use singles GADTs rather than a unique universe Format with
      multiple constructors

--- src/Format/Base.hs
+++ src/Format/Base.hs
@@ -16,6 +16,7 @@
 
 module Format.Base where
 
+import Prelude ((.))
 import Control.Isomorphism.Partial
 import Data.HList
 import Control.Applicative
@@ -23,76 +24,79 @@ import GHC.Exts
 
 -- | 'SFormat' stands for Single Format and represents a 'Format'
 -- containing only one target type.
-type SFormat m i a = Format m i '[ a ]
+type SFormat c m i a = Format c m i '[ a ]
 
 --data Format c (m :: * -> *) (i :: *) (xs :: [ * ]) where
 --  Format :: c m i xs a => a m i xs -> Format c m i xs
  
-data Format (m :: * -> *) (i :: *) (xs :: [ * ]) where
-  Seq :: Format m i xs -> Format m i ys -> Format m i (Append xs ys)
-  Token :: Format m i '[i]
-  CFormat :: Iso args xs -> Format m i args -> Format m i xs
-  Alt :: Format m i xs -> Format m i xs -> Format m i xs
-  Fail :: SList xs -> Format m i xs
-  Pure :: HList xs -> Format m i xs -- Not sure why we would need equality here for printing
-  Bind :: SList ys -> Format m i xs -> (HList xs -> Format m i ys) -> Format m i (Append xs ys)
+--data Format (m :: * -> *) (i :: *) (xs :: [ * ]) where
+--  Seq :: Format m i xs -> Format m i ys -> Format m i (Append xs ys)
+--  Token :: Format m i '[i]
+--  FMap :: Iso args xs -> Format m i args -> Format m i xs
+--  Alt :: Format m i xs -> Format m i xs -> Format m i xs
+--  Fail :: SList xs -> Format m i xs
+--  Pure :: HList xs -> Format m i xs -- Not sure why we would need equality here for printing
+--  Bind :: SList ys -> Format m i xs -> (HList xs -> Format m i ys) -> Format m i (Append xs ys)
 
-data Seq' c (m :: * -> *) (i :: *) (zs :: [*]) where
-  Seq' :: (c m i xs a, c m i ys b, Reify (a m i), Reify (b m i)) => a m i xs -> b m i ys -> Seq' c m i (Append xs ys)
+data Format c (m :: * -> *) (i :: *) (xs :: [*]) where
+  Format :: c m i xs a => a m i xs -> Format c m i xs
 
-data Token' (c :: (* -> *) -> * -> [ * ] -> ((* -> *) -> * -> [*] -> *) -> Constraint) (m :: * -> *) (i :: *) (xs :: [ * ]) where
-  Token' :: Token' c m i '[i]
+data Seq c (m :: * -> *) (i :: *) (zs :: [*]) where
+  Seq :: (c m i xs a, Reify (a m i), 
+          c m i ys b, Reify (b m i)) => a m i xs -> b m i ys -> Seq c m i (Append xs ys)
+
+data Token (c :: (* -> *) -> * -> [ * ] -> ((* -> *) -> * -> [*] -> *) -> Constraint)
+           (m :: * -> *) (i :: *) (xs :: [ * ]) where
+  Token :: Token c m i '[i]
   
-data CFormat' c (m :: * -> *) (i :: *) (xs :: [ * ]) where
-  CFormat' :: (c m i args a) => Iso args xs -> a m i args -> CFormat' c m i xs
+data FMap c (m :: * -> *) (i :: *) (xs :: [ * ]) where
+  FMap :: (c m i args a) => Iso args xs -> a m i args -> FMap c m i xs
  
-data Alt' c (m :: * -> *) (i :: *) (xs :: [ * ]) where
-  Alt' :: (c m i xs a, c m i xs b, Reify (a m i)) => a m i xs -> b m i xs -> Alt' c m i xs
+data Alt c (m :: * -> *) (i :: *) (xs :: [ * ]) where
+  Alt :: (c m i xs a, c m i xs b, Reify (a m i)) => a m i xs -> b m i xs -> Alt c m i xs
 
-seq' :: (Use a c m i xs, Use b c m i ys, Reify (a c m i), Reify (b c m i)) => a c m i xs -> b c m i ys -> Seq' c m i (Append xs ys)
-seq' = Seq'
+data Pure c (m :: * -> *) (i :: *) (xs :: [ * ]) where
+  Pure :: HList xs -> Pure c m i xs
 
-alt' :: (Use a c m i xs, Use b c m i xs, Reify (a c m i)) => a c m i xs -> b c m i xs -> Alt' c m i xs
-alt' = Alt'
+(<*>) :: (Use a c m i xs, Use b c m i ys, Reify (a c m i), Reify (b c m i)) => a c m i xs -> b c m i ys -> Seq c m i (Append xs ys)
+(<*>) = Seq
 
-foo :: Use Token' c m i '[i] => Seq' c m i '[i , i]
-foo = seq' Token' Token'
+(<|>) :: (Use Alt c m i xs, 
+          Use a c m i xs, 
+          Use b c m i xs, 
+          Reify (a c m i)) => a c m i xs -> b c m i xs -> Format c m i xs
+p <|> q = format (Alt p q)
 
-bar :: (Use Token' c m i '[i] ,
-        Use Seq' c m i '[i, i]) => Seq' c m i '[i, i, i, i] 
-bar = seq' foo foo
+token :: Use Token c m i '[i] => Format c m i '[i]
+token = format Token
 
-foobar :: (Use Token' c m i '[i] , 
-           Use Seq' c m i '[i, i],
-           Use Seq' c m i '[i, i, i, i]) => Alt' c m i '[i, i, i ,i]
-foobar = alt' bar bar
+(<$>) :: (Use FMap c m i xs, Use a c m i args) => Iso args xs -> a c m i args -> Format c m i xs
+f <$> x = format (FMap f x)
+
+pure :: Use Pure c m i xs => HList xs -> Format c m i xs
+pure = format . Pure
+
+format :: Use a c m i xs => a c m i xs -> Format c m i xs
+format = Format
 
 type Use a c m i xs = c m i xs (a c)
 
+instance Reify (Token c m i) where
+  toSList Token = SCons SNil
+
+instance Reify (Seq c m i) where
+  toSList (Seq a b) = sappend (toSList a) (toSList b)
+
+instance Reify (Alt c m i) where
+  toSList (Alt a b) = toSList a
+
+instance Reify (FMap c m i) where
+  toSList (FMap i f) = sunapply i
+
+--------------------------------------------------------------------------------
+-- TODO : move to different module
 class ParseWith (m :: * -> *) (i :: *) (xs :: [ * ]) a where
   mkParser' :: a m i xs -> m (HList xs)
  
 class PrintWith s (m :: * -> *) (i :: *) (xs :: [ * ]) a where
   mkPrinter' :: a m i xs -> HList xs -> m s
-
-instance Reify (Token' c m i) where
-  toSList Token' = SCons SNil
-
-instance Reify (Seq' c m i) where
-  toSList (Seq' a b) = sappend (toSList a) (toSList b)
-
-instance Reify (Alt' c m i) where
-  toSList (Alt' a b) = toSList a
-
-instance Reify (CFormat' c m i) where
-  toSList (CFormat' i f) = sunapply i
-
---------------------------------------------------------------------------------
-instance Reify (Format m i) where
-  toSList (Seq f1 f2) = sappend (toSList f1) (toSList f2)
-  toSList (CFormat i _) = sunapply i
-  toSList (Alt f1 f2) = toSList f1
-  toSList (Fail s) = s
-  toSList (Pure hs) = toSList hs
-  toSList Token = SCons SNil
-  toSList (Bind s f k) = sappend (toSList f) s
