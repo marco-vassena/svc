3f0086958641df4d2c5fd40c9df19364ff72cf33
Marco Vassena <m.vassena@students.uu.nl>
2015-01-26 17:12:39 +0100

    Refactoring for NetPbm example

--- examples/NetPbm.hs
+++ examples/NetPbm.hs
@@ -28,47 +28,57 @@ import Text.Parsec (Parsec, eof, parse)
 
 import Util
 
--- TODO move to char module
-spaces :: (Use Satisfy  c m Char, 
-           AlternativeC c m Char) 
-       => Format c m Char '[]
-spaces = some (char ' ' <|> char '\t' <|> char '\n')
+whitespace :: (Use Satisfy  c m Char, 
+               AlternativeC c m Char) 
+            => Format c m Char '[]
+whitespace = some (char ' ' <|> char '\t' <|> char '\n')
 
-data Pbm = Pbm Int Int [[Char]]
-  deriving Show
-
-pbm :: Iso '[Int, Int, [[Char]]] '[Pbm]
-pbm = Iso (Just . hsingleton . happly Pbm) f (SCons (SCons (SCons SNil))) (SCons SNil)
-  where f :: PFunction '[Pbm] '[Int, Int, [[Char]]]
-        f (Cons (Pbm n m img) _) = Just $ Cons n (Cons m (Cons img Nil))
-
-pbmFormat :: (Use Bind     c m Char, 
-              Use Satisfy  c m Char, 
-              AlternativeC c m Char) 
-          => SFormat c m Char Pbm
-pbmFormat = pbm <$> pbmRawFormat
-
-pbmRawFormat :: (Use Bind     c m Char, 
-                 Use Satisfy  c m Char, 
-                 AlternativeC c m Char) 
-             => Format c m Char '[Int, Int, [[Char]]]
-pbmRawFormat = pbmHeader >>= \(Cons n (Cons m _)) -> img n m
-
-pbmHeader :: (AlternativeC c m Char, 
-              Use Satisfy c m Char) 
-          => Format c m Char '[Int, Int]
-pbmHeader = (string "P1\n" *> comment *> int <* spaces) <*> int <* spaces 
-  
+-- | Recognizes a comment and discards its value. 
 comment :: (AlternativeC c m Char, 
             Use Satisfy c m Char) 
         => Format c m Char '[]
 comment = ignore (hsingleton Nothing) <$> optional cm
   where cm = char '#' *> manyTill (satisfy (/= '\n')) (char '\n')
 
+
+--------------------------------------------------------------------------------
+data Pbm = Pbm Int Int [[Char]]
+  deriving Show
+
+-- | Partial isomorphism for 'Pbm'
+pbm :: Iso '[Int, Int, [[Char]]] '[Pbm]
+pbm = Iso (Just . hsingleton . happly Pbm) f (SCons (SCons (SCons SNil))) (SCons SNil)
+  where f :: PFunction '[Pbm] '[Int, Int, [[Char]]]
+        f (Cons (Pbm n m img) _) = Just $ Cons n (Cons m (Cons img Nil))
+--------------------------------------------------------------------------------
+
+-- | Format that targets the 'Pbm' data type.
+pbmFormat :: (Use Bind     c m Char, 
+              Use Satisfy  c m Char, 
+              AlternativeC c m Char) 
+          => SFormat c m Char Pbm
+pbmFormat = pbm <$> pbmRawFormat
+
+-- | Format that recognizes the essential elements of a pbm image.
+pbmRawFormat :: (Use Bind     c m Char, 
+                 Use Satisfy  c m Char, 
+                 AlternativeC c m Char) 
+             => Format c m Char '[Int, Int, [[Char]]]
+pbmRawFormat = pbmHeader >>= \(Cons n (Cons m _)) -> img n m
+
+-- | Recognizes the pbm header and the dimensions of the image
+pbmHeader :: (AlternativeC c m Char, 
+              Use Satisfy c m Char) 
+          => Format c m Char '[Int, Int]
+pbmHeader = (string "P1\n" *> comment *> int <* whitespace) <*> int <* whitespace 
+
+-- | Recognizes a table of space-separated bits 
 img :: (Use Satisfy c m Char, AlternativeC c m Char) 
     => Int -> Int -> SFormat c m Char [[Char]]
-img n m = count n (count m (bit <* spaces))
+img n m = count n (count m (bit <* whitespace))
   where bit = oneOf "01"
+
+--------------------------------------------------------------------------------
 
 pbmParser :: Parsec ByteString () Pbm
 pbmParser = hHead A.<$> (mkParser pbmFormat) A.<* eof
@@ -76,6 +86,7 @@ pbmParser = hHead A.<$> (mkParser pbmFormat) A.<* eof
 pbmPrinter :: Pbm -> Maybe ByteString
 pbmPrinter = mkPrinter pbmFormat . hsingleton
 
+-- | Parse the input and prints the resulting Pbm, then it serializes it again.
 roundtrip :: ByteString -> IO ByteString
 roundtrip s = do 
   case parse pbmParser "" s of
