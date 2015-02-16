d8cea697d35a2b6d6d49af8c6f778b6c11897e2f
Marco Vassena <m.vassena@students.uu.nl>
2015-02-16 11:29:35 +0100

    Second attemp - Monadic repository

--- src/Repository.hs
+++ src/Repository.hs
@@ -2,56 +2,153 @@
 
 module Repository where
 
+import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Typeable
 import Generics.Instant.TH
 import Generics.Instant.GDiff
+import Control.Monad.Except
+import Control.Monad.Trans.State.Lazy
 
-data Repo a where
-  Repo :: GDiff a => [EditScript] -> Maybe a -> Repo a
+-- Branch name
+type BName = String
 
--- The empty repository:
+-- Reference to parents commits
+type Parents a = [HashId]
+
+-- An integer representing the Hash of the EditScript
+-- used to uniquely identify commits.
+type HashId = Int
+
+-- Being Haskell a pure language we are not just 
+-- making referencies to parents, but actually copies.
+-- This may be inacceptable. Sharing must be preserved
+-- ** especially ** when they are serialized.
+data Commit a = Commit { hashId :: HashId,
+                         delta :: EditScript,
+                         parents :: Parents a}
+  deriving Show
+
+data Head a = Head { branch :: BName,
+                     value  :: (Maybe a) }
+  deriving Show
+
+data RState a = RState { branches :: Map BName [Commit a],
+                         tip :: (Head a) }
+
+data RError = BranchNotFound String
+  deriving Show
+
+type Repo a b = StateT (RState a) (Except RError) b
+
+runRepo :: Repo a b -> Either RError b
+runRepo r = runExcept (evalStateT r initState)
+  where initState = RState M.empty (Head "" Nothing)
+
+-- Adds a branch with the given name
+-- TODO fail if the branch is already present
+newBranch :: BName -> Repo a ()
+newBranch b = modify f
+  where f (RState m h) = RState (M.insert b [] m) h
+
+getBranch :: BName -> Repo a [Commit a]
+getBranch b =  do
+  m <- gets branches
+  case M.lookup b m of
+    Just cs -> return cs
+    Nothing -> lift (throwError (BranchNotFound b))
+
+getHead :: Repo a (Head a)
+getHead = gets tip
+
+putHead :: Head a -> Repo a ()
+putHead h = modify f
+  where f (RState m _) = RState m h
+
+addCommit :: BName -> Commit a -> Repo a ()
+addCommit b c = modify f
+  where f (RState m h) = RState (M.adjust (c:) b m) h
+
+setHead :: (Show a, GDiff a) => BName -> Repo a ()
+setHead b = do
+  cs <- getBranch b >>= \cs -> return [delta c | c <- cs]
+  let v = foldr unsafePatch Nothing cs
+  putHead (Head b v)
+
+-- Change branch
+switch :: (Show a, GDiff a) => BName -> Repo a ()
+switch b = setHead b
+
+getValue :: Repo a (Maybe a)
+getValue = gets (value . tip)
+  
+
+-- Initialize repository:
+--- * master branch
 --  * empty history
 --  * no value
-empty :: GDiff a => Repo a
-empty = Repo [] Nothing
+initRepo :: (Show a, GDiff a) => Repo a ()
+initRepo = do
+  newBranch "master"
+  setHead "master"
+
+getParents b = do
+  cs <- getBranch b
+  case cs of
+    [] -> return []
+    (c:cs) -> return [hashId c]
 
 -- Produces a new Repo containing the new value,
 -- and keeps track in the history of the deltas
-commit :: GDiff a => a -> Repo a -> Repo a
-commit y (Repo hs x) = Repo (dF : hs) (Just y)
-  where dF = diff x (Just y)
-
--- @back n r@ rolls back @n@ commits in the repo @r@
--- The returned repo is some kind of "detached state",
--- where the previous history is available, but 
--- the future commits are discarded.
-back :: Int -> Repo a -> Repo a
-back n (Repo hs v) = Repo past (foldr unsafePatch Nothing past)
-  where (future, past) = splitAt n hs
-
--- Returns the n-th version of the repo.
--- (0-th is the empty repo).
-version :: Int -> Repo a -> Repo a
-version n r@(Repo hs _) = back (l - n) r
-  where l = length hs
+commit :: GDiff a => a -> Repo a ()
+commit y = do
+  Head b x <- getHead
+  ps <- getParents b
+  let d = diff x (Just y)
+  let newCommit = Commit 0 d ps
+  let newHead = Head b (Just y) 
+  addCommit b newCommit
+  putHead newHead
+  
+---- @back n r@ rolls back @n@ commits in the repo @r@
+---- The returned repo is some kind of "detached state",
+---- where the previous history is available, but 
+---- the future commits are discarded.
+----back :: Int -> Repo a -> Repo a
+----back n (Repo hs v) = Repo past (foldr unsafePatch Nothing past)
+----  where (future, past) = splitAt n hs
+--
+------ Returns the n-th version of the repo.
+------ (0-th is the empty repo).
+----version :: Int -> Repo a -> Repo a
+----version n r@(Repo hs _) = back (l - n) r
+----  where l = length hs
 
 -- Calls patch and throws error if it fails
-unsafePatch :: GDiff a => EditScript -> a -> a
+unsafePatch :: (GDiff a, Show a) => EditScript -> a -> a
 unsafePatch d x = 
   case patch d x of
     Just y -> y
     Nothing -> error $ "patch:\t" ++ show d ++ "\nfailed over:\t" ++ show x
 
-r0, r1, r2, r3 :: Repo Int
-r0 = empty
-r1 = commit 1 r0
-r2 = commit 2 r1
-r3 = commit 3 r2
 
--- Branching is the result of commiting from a previous state
--- b2 shares the first commits with r2, but the last differs.
-b2 :: Repo Int
-b2 = commit 4 r1
+example0 :: Repo Int (Maybe Int)
+example0 = do
+  initRepo
+  getValue
+
+example1 :: Repo Int (Maybe Int)
+example1 = do
+  initRepo
+  commit 1
+  commit 2
+  getValue
+
+---- Branching is the result of commiting from a previous state
+---- b2 shares the first commits with r2, but the last differs.
+--b2 :: Repo Int
+--b2 = commit 4 r1
 
 --------------------------------------------------------------------------------
 -- Auxiliary GDiff instances
@@ -76,7 +173,3 @@ instance Typeable a => Build (Maybe a)  where
 instance GDiff a => GDiff (Maybe a)
 
 --------------------------------------------------------------------------------
--- Debugging
-
-instance Show (Repo a) where
-  show (Repo hs v) = unwords ["Repo", show hs, show v]
