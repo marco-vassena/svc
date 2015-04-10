546cdbbe020b64a3e19e23940f839ac81b2d5bc4
Marco Vassena <m.vassena@students.uu.nl>
2015-04-10 09:11:20 +0200

    First attempt to define diff3 using tree-shaped scripts

---
+++ src/Repo/Diff3.hs
@@ -0,0 +1,142 @@
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Repo.Diff3 where
+
+import GHC.Generics (Generic)
+import Generics.Instant.TH
+import qualified Generics.Instant.GDiff as G (diff)
+import Generics.Instant.GDiff hiding (diff, Del, Ins, Cpy)
+import Data.Typeable
+import Data.Hashable
+
+data Tree a = Node a [Tree a]
+  deriving (Show, Eq, Typeable, Generic)
+
+leaf :: a -> Tree a
+leaf x = Node x []
+
+class Metric a where
+  -- Laws:
+  --  d x y = d y x                  (symmetry)
+  --  d x y >= 0                          (non-negativity)
+  --  d x x = 0                           (identity)
+  --  d x z <= d x y + d y z    (triangle inequality)
+  distance :: a -> a -> Double
+
+instance Metric Char where
+  distance x y = if x == y then 0 else 1
+
+instance Metric Int where
+  distance x y = if x == y then 0 else 1
+
+data ETree a = Ch2 Double a a [ETree a]
+             | Ins a [ETree a]
+             | Del a [ETree a]
+  deriving (Show, Eq)
+
+-- At the moment we are not considering the size of the trees
+cost :: ETree a -> Double
+cost (Ch2 d _ _ es) = d + costs es
+cost (Del _ es) = 1 + costs es
+cost (Ins _ es) = 1 + costs es
+
+costs :: [ETree a] -> Double
+costs = sum . map cost
+
+(&) :: ETree a -> ETree a -> ETree a
+a & b = if cost a <= cost b then a else b
+
+(&&&) :: [ETree a] -> [ETree a] -> [ETree a]
+as &&& bs = if costs as <= costs bs then as else bs
+
+-- TODO memoization
+diff :: Metric a => Tree a -> Tree a -> ETree a
+diff n@(Node x xs) m@(Node y ys) = a & b & c
+  where a = Del x (diffs xs [m])
+        b = Ins y (diffs [n] ys) 
+        c = Ch2 (distance x y) x y (diffs xs ys)
+
+diffs :: Metric a => [Tree a] -> [Tree a] -> [ETree a]
+diffs [] [] = []
+diffs (x:xs) [] = del x : diffs xs []
+diffs [] (y:ys) = ins y : diffs [] ys
+diffs (x:xs) (y:ys) = a &&& b &&& c
+  where a = diff x y : diffs xs ys
+        b = del x : diffs xs (y:ys)
+        c = ins y : diffs (x:xs) ys
+
+ins, del :: Tree a -> ETree a
+ins (Node x xs) = Ins x (map ins xs) 
+del (Node x xs) = Del x (map del xs)
+
+-- TODO 
+-- diff3 (how ETrees are compared for conflicts?)
+-- EList (ConsA / ConsD / ConsC)
+
+-- Generic part
+-- More interesting use case lists of lists of integers
+-- > We need a more refined tree structure : nodes and leaves 
+--   may contain arbitrary types and constructors
+-- Issues:
+--  > Subtrees with different types may be compared
+--  > distance requires two elements of the same type (no existential)
+
+-- The nice properties of the Tree view are
+-- 1) Precise representation of embedding (shape change vs value change)
+-- 2) It's easy to compare 2 edits to find conflicts.
+
+-- In order to maintain the tree structure we cannot use simply Cpy, but 
+-- we need Ch2 which encodes Cpy (when distance is 0) or a value change.
+-- Without that we could not match properly examples like
+-- 1          0
+-- | \        | \
+-- 2  3       2  3
+-- Because we need to call diffs on the children.
+
+-- Example with l1 l2 l3 : why is [1,4,5,2,3,6] a good resolution?
+--  1) It preserves most of the ordering (embedding).
+--  2) Values are not duplicated
+--  3) Minimizes the changes to shape
+--------------------------------------------------------------------------------
+-- Example
+
+t1, t2, t3 :: Tree Char
+
+t1 = Node 'a' [Node 'b' [],
+               Node 'c' [Node 'd' [],
+                         Node 'e' []]]
+
+t2 = Node 'a' [Node 'd' [Node 'b' []],
+               Node 'e' []]
+
+t3 = Node 'a' [Node 'b' [Node 'g' []],
+               Node 'f' [Node 'd' [],
+                         Node 'e' []]]
+
+node :: a -> Tree a -> Tree a
+node x t = Node x [t]
+
+l1, l2, l3 :: Tree Int
+l1 = foldr node (leaf 6) [1,2,3,4,5]
+l2 = foldr node (leaf 6) [1,4,5,2,3]
+l3 = foldr node (leaf 6) [1,2,4,5,3]
+
+--------------------------------------------------------------------------------
+-- GDiff for Tree
+
+$(deriveAll ''Tree)
+instance (Show a, GDiff a) => SEq      (Tree a) where shallowEq  = shallowEqDef
+instance (Show a, GDiff a) => Build    (Tree a) where build      = buildDef
+instance (Show a, GDiff a) => Children (Tree a) where children   = childrenDef
+instance (Show a, GDiff a) => GDiff (Tree a)
+
+instance Show a => Pretty (Tree a) where
+  pretty (Node x _) = "Node " ++ show x
+
+instance Hashable a => Hashable (Tree a) where
+
+x = Node 'a' [leaf 'b', leaf 'c']
+y = Node 'd' [leaf 'b', Node 'c' [leaf 'e']]
--- src/Repo/Head.hs
+++ src/Repo/Head.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MagicHash #-}
+
 module Repo.Head where
 
+import Data.Hashable
+import GHC.Exts
+import GHC.Prim
 import Generics.Instant.GDiff
 import Repo.Path
+import Debug.Trace
 
 -- Abstract data type that tracks the diff of an object in a Path and 
 -- maintain the latest version of the object
@@ -48,3 +55,151 @@ mergeWith :: GDiff a => a -> Path a -> Path a -> Path a
 mergeWith x p1 p2 = merge p1 p2 (diff v x)
   where v = value h 
         h = mkHeadFromLca (lca p1 p2) id
+
+-- An edit script produced by diff3
+data EDiff3 = Cons Edit EDiff3
+            | Conflict [Edit] [Edit] EDiff3 -- TODO use single Edit rather than list
+            | End
+  deriving Show
+
+toEDiff3 :: [Edit] -> EDiff3
+toEDiff3 = foldr Cons End
+
+append :: EDiff3 -> EDiff3 -> EDiff3
+append End ys = ys
+append (Cons x xs) ys = Cons x (append xs ys)
+append (Conflict c1 c2 xs) ys = Conflict c1 c2 (append xs ys)
+
+-- TODO probably the interface should be :
+--    diff3 :: GDiff a => a -> a -> a -> EDiff3
+-- TODO here I should check if one or both are actually the same as the ancestor
+diff3 :: EditScript -> EditScript -> EDiff3
+diff3 (ES _ xs) (ES _ ys) = diff3' xs ys  
+
+hashOf :: Edit -> Int
+hashOf (Cpy x) = hash x
+hashOf (Ins x) = hash x
+hashOf (Del x) = hash x
+
+-- * I don't think that this can work properly when we have actually
+--   structured data. The script is flat (list).
+-- * I don't have any guarantee that the script produced will be applicable
+--   to the original
+diff3' :: [Edit] -> [Edit] -> EDiff3
+diff3' []     []     = End
+diff3' []     xs     = Conflict [] xs End
+diff3' xs     []     = Conflict xs [] End
+diff3' (x:xs) (y:ys) | hash x == hash y = Cons x (diff3' xs ys)
+diff3' (Ins x : xs) (Ins y : ys) = Conflict [Ins x] [Ins y] (diff3' xs ys)
+diff3' (Ins x : xs) (Cpy y : ys) = Cons (Ins x) (diff3' xs (Cpy y : ys))
+diff3' (Cpy x : xs) (Ins y : ys) = Cons (Ins y) (diff3' (Cpy x : xs) ys)
+diff3' a@(Del x : xs) b@(Cpy y : ys) =
+  case span (\e -> hash e /= hash (Ins x)) a of
+    (_, []) -> Conflict a b End
+    (ps, _:ss) -> 
+      case align ss ys of
+        ([], [], as, bs) -> toEDiff3 ps `append` Cons (Ins x) (diff3' as bs)
+        (c1, c2, as, bs) -> toEDiff3 ps `append` Cons (Ins x) (Conflict c1 (map cpy2Ins c2) (diff3' as bs))
+diff3' (Cpy x : xs) (Del y : ys) = diff3' (Del y : ys) (Cpy x : xs)
+diff3' a b = error $ "woops " ++ show a ++ " " ++ show b
+
+--align :: [Edit] -> [Edit] -> 
+align xs ys = go [] [] xs ys
+  where go c1 c2 a@(Cpy x : xs) b@(Cpy y : ys) | hash x == hash y = (reverse c1, reverse c2, a, b)
+        go c1 c2 a@(Cpy x:xs) (y:ys) = go c1 (y:c2) a ys
+        go c1 c2 (x:xs) (y:ys) = go (x:c1) (y:c2) xs ys
+        go c1 c2 xs     []     = (reverse c1, reverse c2, xs, [])
+        go c1 c2 []     ys     = (reverse c1, reverse c2, [], ys)
+
+
+-- TODO what should happen for Ins / Del
+
+cpy2Ins :: Edit -> Edit
+cpy2Ins (Cpy x) = Ins x
+cpy2Ins e       = e
+
+data Strategy = Their
+              | Mine
+  deriving (Show, Eq)
+
+solveWithStrategy :: Strategy -> EDiff3 -> [Edit]
+solveWithStrategy s e = go e
+  where go End = []
+        go (Cons e es) = e : go es
+        go (Conflict mine their es) = 
+          case s of 
+            Mine -> mine ++ go es
+            Their -> their ++ go es
+
+es :: [Edit] -> EditScript
+es xs = ES l xs
+  where !(I# l) = length xs
+
+a,o,b :: [Int]
+o = [1,2,3,4,5,6]
+a = [1,4,5,2,3,6]
+b = [1,2,4,5,3,6]
+
+cb = [1,4,5,2,4,5,3,6]
+ca = [1,4,5,2,3,6]
+co = [1,4,5,2,3,4,5,6]
+
+oa = diff o a
+ob = diff o b
+oab = diff3 oa ob
+
+ocb = diff o cb
+oca = diff o ca
+oco = diff o co
+
+
+--------------------------------------------------------------------------------
+
+x,y,z :: [Int]
+x = [1,2,3,4,5,6,7]
+y = [1,4,5,2,3,6,9,7]
+z = [1,2,4,5,3,6,8,7]
+
+xy = diff x y
+xz = diff x z
+
+prettyDiff :: EditScript -> EditScript -> String
+prettyDiff (ES _ xs) (ES _ ys) = unlines $ f xs ys
+  where f :: [Edit] -> [Edit] -> [String]
+        f xs [] = map show xs
+        f [] ys = zipWith (++) (repeat (spcs 0)) (map show ys)
+        f (x:xs) (y:ys) = (show x ++ spcs (length (show x)) ++ show y) : f xs ys
+        spcs n = replicate (50 - n) ' '
+--diff x y
+-- ES 9 [Cpy [1,2,3,4],Cpy 1,Cpy [2,3,4],Cpy 2,Cpy [3,4],Del 3,Del [4],Cpy 4,Cpy []]
+
+-- diff x z
+-- ES 12 [Cpy [1,2,3,4],Cpy 1,Cpy [2,3,4],Del 2,Ins 4,Cpy [3,4],Del 3,Ins 5,Cpy [4],Del 4,Ins 2,Cpy []]
+
+--dxyz :: EditScript
+--dxyz = ES l xs
+--  where !(ES l1 xy) = diff x y
+--        !(ES l2 xz) = diff x z
+--        xs = take 11 xz ++ drop 4 xy
+--        !(I# l) = length xs
+
+--xs = [cpy [1,2,3,4],cpy 1,cpy [2,3,4],del 2,ins 4,cpy [3,4],del 3,ins 5,cpy [4],del 4,ins 2,
+--              cpy [3,4],del 3,del [4],cpy 4,cpy []]
+
+cpy :: GDiff a => a -> Edit
+cpy = Cpy . Ex
+
+ins :: GDiff a => a -> Edit
+ins = Ins . Ex
+
+del :: GDiff a => a -> Edit
+del = Del . Ex
+
+--a, o, b :: [Int]
+--a = [1,4,5,2,3,6]
+--o = [1,2,3,4,5,6]
+--b = [1,2,4,5,3,6]
+--
+--ao = diff o a
+--bo = diff o b
+
