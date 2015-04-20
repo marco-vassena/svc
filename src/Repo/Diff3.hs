624607b4d804d8aa64e53c5674ecb9be29f147ab
Marco Vassena <m.vassena@students.uu.nl>
2015-04-20 15:21:08 +0200

    Working on diff3 prototype
      - Distance measured in Int
      - Fixed Show instances
    
    TODO
      - Alignment

--- src/Repo/Diff3.hs
+++ src/Repo/Diff3.hs
@@ -34,13 +34,14 @@ class TreeLike f a where
 
 --------------------------------------------------------------------------------
 class Metric f where
-  distance :: f xs a -> f ys a -> Double
+  distance :: f xs a -> f ys a -> Int
 
 -- | The ETree datatype represents a tree-shaped well-typed edit script
-data ETree a where
-  Ins :: Family f => f xs a -> EList ETree xs -> ETree a
-  Del :: Family f => f xs a -> EList ETree '[ a ] -> ETree a
-  Upd :: (Family f, Metric f) => f xs a -> f ys a -> EList ETree ys -> ETree a  -- TODO add distance
+-- TODO we can remove constraint about f since we expose it
+data ETree f a where
+  Ins :: (Family f) => f xs a -> EList (ETree f) xs -> ETree f a
+  Del :: Family f => f xs a -> EList (ETree f) '[ a ] -> ETree f a
+  Upd :: (Family f, Metric f) => f xs a -> f ys a -> EList (ETree f) ys -> ETree f a  -- TODO add distance
 
 data EList f xs where
   ENil :: EList f '[]
@@ -56,24 +57,24 @@ emap f (ConsU x xs) = f x : emap f xs
 
 
 -- TODO probably we want to store the cost with the ETree
-cost :: ETree a -> Double
+cost :: ETree f a -> Int
 cost (Ins _ xs) = 1 + costs xs
 cost (Del _ xs) = 1 + costs xs
 cost (Upd x y xs) = distance x y + costs xs
 
-costs :: EList ETree xs -> Double
+costs :: EList (ETree f) xs -> Int
 costs xs = sum (emap cost xs)
 
 -- Returns the best edit tree (least distance)
-(&) :: ETree a -> ETree a -> ETree a
+(&) :: ETree f a -> ETree f a -> ETree f a
 x & y = if cost x <= cost y then x else y
 
 -- Returns the best edit list (least distance)
-(&&&) :: EList ETree xs -> EList ETree xs -> EList ETree xs
+(&&&) :: EList (ETree f) xs -> EList (ETree f) xs -> EList (ETree f) xs
 xs &&& ys = if costs xs <= costs ys then xs else ys
 
 -- TODO memoization
-diff :: Metric f => DTree f a -> DTree f a -> ETree a
+diff :: Metric f => DTree f a -> DTree f a -> ETree f a
 diff n@(Node f xs) m@(Node g ys) =
   case decEq f g of
     Just Refl -> a & b & (Upd f g (diffs xs ys))
@@ -81,7 +82,7 @@ diff n@(Node f xs) m@(Node g ys) =
   where a = Del f (diffs xs (dsingleton m))
         b = Ins g (diffs (dsingleton n) ys)
         
-diffs :: Metric f => DList (DTree f) xs -> DList (DTree f) ys -> EList ETree ys
+diffs :: Metric f => DList (DTree f) xs -> DList (DTree f) ys -> EList (ETree f) ys
 diffs DNil DNil = ENil
 diffs DNil (DCons x xs) = ConsA (ins x) (diffs DNil xs)
 diffs (DCons x xs) DNil = ConsD (ins x) (diffs xs DNil)
@@ -92,10 +93,10 @@ diffs d1@(DCons x xs) d2@(DCons y ys) =
   where a = ConsD (ins x) (diffs xs d2)
         b = ConsA (ins y) (diffs d1 ys)
  
-ins :: DTree f xs -> ETree xs
+ins :: DTree f xs -> ETree f xs
 ins (Node f xs) = Ins f (insList xs)
 
-insList :: DList (DTree f) xs -> EList ETree xs
+insList :: DList (DTree f) xs -> EList (ETree f) xs
 insList DNil = ENil
 insList (DCons x xs) = ConsA (ins x) (insList xs)
 
@@ -104,6 +105,47 @@ dsingleton x = DCons x DNil
 
 decEq' :: DTree f a -> DTree f b -> Maybe (a :~: b)
 decEq' (Node a xs) (Node b ys) = decEq a b
+
+--------------------------------------------------------------------------------
+-- Diff3
+
+
+-- For the time being I am assuming the scripts are "aligned"
+diff3 :: Metric f => ETree f a -> ETree f a -> ETree f a
+diff3 (Upd o x xs) (Upd _ y ys)
+  | distance x y == 0 = Upd o y (diffs3 xs ys) 
+  | otherwise         = conflict x y
+diff3 u@(Upd o x xs) (Ins y ys) = Ins y (align1 u ys)
+diff3 (Ins x xs) u@(Upd o y ys) = Ins x (align2 xs u)
+diff3 (Ins x xs) (Ins y ys) =
+  let c = conflict x y in
+  case decEq x y of
+    Just Refl -> if distance x y == 0 then Ins y (diffs3 xs ys) else c
+    Nothing -> c
+diff3 (Del x xs) (Del y ys) = Del x (diffs3 xs ys) -- We don't check x == y since I am assuing we are aligned
+diff3 (Upd o x xs) (Del y ys)
+  | distance o x == 0 = Del y (diffs3 xs ys)
+  | otherwise         = deletedOrChanged o y x
+diff3 (Del x xs) u2@(Upd o y ys)
+  | distance o y == 0 = Del x undefined -- (DCons (diff3 u1 u2) DNil)
+  | otherwise         = deletedOrChanged o x y 
+
+conflict a b = error msg
+  where msg = "conflict: " ++ string a ++ " <-> " ++ string b
+
+deletedOrChanged o a b = error msg
+  where msg = "conflict: " ++ deleted ++ " <-> " ++ changed
+        deleted = "deleted " ++ string a
+        changed = string o ++ " changed to " ++ string b
+
+align1 :: ETree f a -> EList (ETree f) xs -> EList (ETree f) xs
+align1 = undefined
+
+align2 :: EList (ETree f) xs -> ETree f a -> EList (ETree f) xs
+align2 = undefined
+
+diffs3 :: EList (ETree f) xs -> EList (ETree f) ys -> EList (ETree f) ys
+diffs3 = undefined
 
 --------------------------------------------------------------------------------
 
@@ -122,11 +164,13 @@ class ShowDT f sig where
 instance ShowDT f '[] where
 instance (Show (DTree f a), ShowDT f xs) => ShowDT f (a ': xs) where
 
-instance Show (ETree a) where
-  show (Ins f xs) = unwords ["Ins", string f, showLikeList xs]
-  show (Del f xs) = unwords ["Del", string f, showLikeList xs]
-  show (Upd x y xs) = unwords ["Upd", string x, string y, showLikeList xs]
-  
-showLikeList :: EList ETree xs -> String
-showLikeList args = "[" ++ concat xss ++ "]"
-  where xss = intersperse ", " (emap show args)
+instance Show (ETree f a) where
+  show (Ins f xs) = "Ins " ++ string f ++ " (" ++ show xs ++ ")"
+  show (Del f xs) = "Del " ++ string f ++ " (" ++ show xs ++ ")"
+  show (Upd x y xs) = "Upd " ++ string x ++ " " ++ string y ++ " (" ++ show xs ++ ")"
+
+instance Show (EList (ETree f) xs) where
+  show ENil = "ENil"
+  show (ConsA x xs) = "ConsA (" ++ show x ++ " " ++ show xs ++ ")"
+  show (ConsD x xs) = "ConsD (" ++ show x ++ " " ++ show xs ++ ")"
+  show (ConsU x xs) = "ConsU (" ++ show x ++ " " ++ show xs ++ ")"
